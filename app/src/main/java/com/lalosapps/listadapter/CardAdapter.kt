6ababf2f3f96ddfaecec4380ab2d72a6ea54c3f1
cbarrios <lalobarrios16@yahoo.com>
2022-10-25 18:02:00 -0400

    master: (6) list adapter implementation

---
+++ app/src/main/java/com/lalosapps/listadapter/CardAdapter.kt
@@ -0,0 +1,48 @@
+package com.lalosapps.listadapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.lalosapps.listadapter.databinding.CardItemBinding
+
+class CardAdapter : ListAdapter<CardItem, CardAdapter.CardViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        return CardViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.card_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        val card = getItem(position)
+        holder.bind(card)
+    }
+
+    object DiffCallback : DiffUtil.ItemCallback<CardItem>() {
+        override fun areItemsTheSame(oldItem: CardItem, newItem: CardItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: CardItem, newItem: CardItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class CardViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = CardItemBinding.bind(view)
+
+        fun bind(card: CardItem) {
+            binding.apply {
+                title.text = card.title
+                description.text = card.description
+                val resource =
+                    if (card.favorite) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
+                icon.setImageResource(resource)
+            }
+        }
+    }
+}
--- app/src/main/java/com/lalosapps/listadapter/MainActivity.kt
+++ app/src/main/java/com/lalosapps/listadapter/MainActivity.kt
@@ -7,10 +7,16 @@ import com.lalosapps.listadapter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: CardAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = CardAdapter().apply {
+            submitList(CardProvider.cards)
+        }
+        binding.recycler.adapter = adapter
     }
 }
