1f571552b67db48d409f4446a914890985f27759
cbarrios <lalobarrios16@yahoo.com>
2022-10-25 17:44:22 -0400

    master: (5) default list provided (immutable)

---
+++ app/src/main/java/com/lalosapps/listadapter/CardProvider.kt
@@ -0,0 +1,67 @@
+package com.lalosapps.listadapter
+
+object CardProvider {
+
+    val cards = listOf(
+        CardItem(
+            1,
+            "Title 1",
+            "Description 1",
+            false
+        ),
+        CardItem(
+            2,
+            "Title 2",
+            "Description 2",
+            false
+        ),
+        CardItem(
+            3,
+            "Title 3",
+            "Description 3",
+            false
+        ),
+        CardItem(
+            4,
+            "Title 4",
+            "Description 4",
+            false
+        ),
+        CardItem(
+            5,
+            "Title 5",
+            "Description 5",
+            false
+        ),
+        CardItem(
+            6,
+            "Title 6",
+            "Description 6",
+            false
+        ),
+        CardItem(
+            7,
+            "Title 7",
+            "Description 7",
+            false
+        ),
+        CardItem(
+            8,
+            "Title 8",
+            "Description 8",
+            false
+        ),
+        CardItem(
+            9,
+            "Title 9",
+            "Description 9",
+            false
+        ),
+        CardItem(
+            10,
+            "Title 10",
+            "Description 10",
+            false
+        )
+    )
+}
